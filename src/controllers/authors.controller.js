4ab2a8720e06e0282203d4b2b393df36892d4b82
mgiussano <mgiussanoareco@gmail.com>
2026-06-10 15:32:55 -0300

    feat: agregar rutas y controladores de authors

---
+++ src/controllers/authors.controller.js
@@ -0,0 +1,46 @@
+const authorsService = require('../services/authors.service');
+
+const getAll = async (req, res) => {
+    try {
+        const authors = await authorsService.getAll();
+        res.status(200).json(authors);
+      } catch(error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+const getById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const author = await authorsService.getById(id);
+    if (!author) return res.status(404).json({ error: 'Author no encontrado' });
+    res.status(200).json(author);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const update = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { name, email, bio } = req.body;
+    const updatedAuthor = await authorsService.update(id, { name, email, bio });
+    if (!updatedAuthor) return res.status(404).json({ error: 'Author no encontrado' });
+    res.status(200).json(updatedAuthor);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const remove = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleted = await authorsService.remove(id);
+    if (!deleted) return res.status(404).json({ error: 'Author no encontrado' });
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { getAll, getById, create, update, remove };
--- src/server.js
+++ src/server.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const authorsRouter = require('./routers/authors.router');
 
 const server = express();
 
 server.use(express.json());
+server.use('/authors', authorsRouter);
 
 module.exports = server;
